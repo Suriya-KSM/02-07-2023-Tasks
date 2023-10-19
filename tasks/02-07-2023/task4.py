17df9ca0dad2cd2bbf7255ccd0bd530c2990794a
Suriyamoorthy K <146614893+Suriya-KSM@users.noreply.github.com>
2023-10-19 17:11:36 +0530

    Delete tasks/02-07-2023 directory

--- tasks/02-07-2023/task4.py
+++
@@ -1,9 +0,0 @@
-#To find unique character form string
-
-def unique_char(i):
-    b=set(i)
-    return len(b)
-    
-i="helloworld"
-
-print("Total number of unique characters", unique_char(i))
--- tasks/02-07-2023/task9.py
+++
@@ -1,9 +0,0 @@
-#To find the words count from a string
-
-def wordcount(str):
-
-    print("Number or words in the given string is: ", len(str.split()))
- 
-str=input("Input:")
-
-wordcount(str)
